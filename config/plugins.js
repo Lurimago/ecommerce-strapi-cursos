9b36a14c95a7546f79de7a81add300e478b16da0
Luis Ricardo Marcelo Gomez <lmarcelg@emeal.nttdata.com>
2024-07-04 00:54:48 -0500

    first commit

--- config/plugins.js
+++ config/plugins.js
@@ -3,8 +3,7 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'aws-s3',
         providerOptions: {
-            //  accessKeyId: "AKIAQ3EGV5QFQ5HGGXCO",
-            // secretAccessKey: "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",
+
             region: "us-east-2",
             params: {
               Bucket: "ecommerce-strapi-cursos",
